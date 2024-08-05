dae19eb56a889bb9fbd16a0599ea1b2ba48006d9
Fabiano Martins <fabiano.paula.martins@gmail.com>
2024-08-05 07:25:13 -0300

    refactoring execute folder to file_engine

--- src/execute.rs
+++ src/execute.rs
@@ -1,19 +1,14 @@
-use std::fs::create_dir_all;
 use std::fs::File;
 
 use crate::config::Config;
 use crate::parser::parser;
 use crate::parser::ExecutionCommands;
 
-mod create_table;
-
-fn test_data_file_exists(config: &Config) {
-    let _ = create_dir_all(config.base_path.clone());
-    let _ = File::create(format!("{0}/data.json", config.base_path.clone()));
-}
+use crate::file_engine::create_table;
+use crate::file_engine::helpers;
 
 pub fn execute(config: &Config, filepath: &str, sql: &str) {
-    test_data_file_exists(config);
+    helpers::create_base_folder(config);
 
     let _file = File::create(filepath);
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,7 @@ pub mod config;
 pub mod parser;
 pub mod execute;
 
+pub mod file_engine;
+
 pub mod table;
 pub mod column;
